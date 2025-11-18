24b7635c0b69a561149cee04d0b3431555af12ec
alfitaraissaputri <alfitaraissa@gmail.com>
2025-11-18 10:21:46 +0700

    Commit  tugas pbb id_user + mapping

--- app/Models/Catatan.php
+++ app/Models/Catatan.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Catatan extends Model
 {
+    //HasFactory untuk menambahkan dummy data, gak kepake sekarang tapi biasanya kepake 
+    use HasFactory;
+
     protected $table = 'catatan';
+
+    protected $fillable = [
+        'judul',
+        'isi',
+        'user_id'
+    ];
+
+     public function user()
+    {
+        // Model Catatan ini "milik" (belongsTo) satu User
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,50 @@
 <?php
 
 use App\Models\Catatan;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes - CRUD CATATAN (TUGAS 2)
+|--------------------------------------------------------------------------
+|
+| URL akan menjadi: /api/catatan
+|
+*/
 
 Route::get('/catatan', function (Request $request) {
-    $data = Catatan::get();
-
+    // Tampilkan catatan beserta data pemiliknya
+    $data = Catatan::with('user')->get();
     return response()->json($data);
 });
 
 Route::post('/catatan', function (Request $request) {
+    // Validasi
+    $request->validate([
+        'judul' => 'required',
+        'isi' => 'required',
+        'user_id' => 'required|exists:users,id' // Memastikan user_id ada & valid
+    ]);
+
     $catatan = new Catatan();
     $catatan->judul = $request->input('judul');
     $catatan->isi = $request->input('isi');
+    $catatan->user_id = $request->input('user_id'); // Simpan ID pemilik
     $catatan->save();
 
-    return response()->json($catatan);
+    return response()->json($catatan, 201); // 201 = Created
 });
 
 Route::get('/catatan/{id}', function ($id) {
-    $catatan = Catatan::find($id);
+    // Tampilkan 1 catatan beserta data pemiliknya
+    $catatan = Catatan::with('user')->find($id);
+
+    if (!$catatan) {
+        return response()->json(['message' => 'Catatan tidak ditemukan'], 404);
+    }
 
     return response()->json($catatan);
 });
@@ -31,6 +55,12 @@ Route::put('/catatan/{id}', function (Request $request, $id) {
     if ($catatan) {
         $catatan->judul = $request->input('judul');
         $catatan->isi = $request->input('isi');
+        
+        // Update user_id jika dikirim
+        if ($request->has('user_id')) {
+            $catatan->user_id = $request->input('user_id');
+        }
+        
         $catatan->save();
     } else {
         return response()->json([
@@ -57,6 +87,80 @@ Route::delete('/catatan/{id}', function ($id) {
     ]);
 });
 
-// bikin api untuk user
-// crud user, kemudian maping catatan ke user tertentu
-// modifikasi crud catatan dengan menambahkan user sebagai pemilik catatan
+//Zona User
+
+// Get semua user
+Route::get('/users', function() {
+    $users = User::all();
+    return response()->json($users);
+});
+
+// Buat user baru
+Route::post('/users', function(Request $request) {
+    $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|string|min:8'
+    ]);
+
+    $user = new User();
+    $user->name = $request->input('name');
+    $user->email = $request->input('email');
+    $user->password = Hash::make($request->input('password')); // Hash password
+    $user->save();
+
+    return response()->json($user, 201);
+});
+
+// Get 1 user berdasarkan ID
+Route::get('/users/{id}', function($id) {
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User tidak ditemukan'], 404);
+    }
+    return response()->json($user);
+});
+
+// Update user
+Route::put('/users/{id}', function(Request $request, $id) {
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User tidak ditemukan'], 404);
+    }
+
+    $user->name = $request->input('name');
+    $user->email = $request->input('email');
+    // Update password hanya jika diisi
+    if ($request->has('password') && $request->input('password') != '') {
+        $user->password = Hash::make($request->input('password'));
+    }
+    $user->save();
+
+    return response()->json($user);
+});
+
+// Hapus user
+Route::delete('/users/{id}', function($id) {
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User tidak ditemukan'], 404);
+    }
+    $user->delete();
+    return response()->json(['message' => 'User berhasil dihapus']);
+});
+
+
+//Mapping User dengan Catatan
+
+// Rute untuk melihat semua catatan milik user tertentu
+Route::get('/users/{id}/catatan', function($id) {
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User tidak ditemukan'], 404);
+    }
+
+    // Ambil user beserta relasi "catatans"
+    $userWithCatatan = User::with('catatans')->find($id);
+
+    return response()->json($userWithCatatan);
+});
